b266d4dde04dd14ffb574e26e3a2ff4c8725b616
Mathis Delaunay <delaunaymathis@yahoo.fr>
2018-04-26 23:54:27 +0200

    Adding the home page

---
+++ src/Controller/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mathisdelaunay
+ * Date: 26/04/2018
+ * Time: 22:02
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/", name="main")
+     */
+    public function index()
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
